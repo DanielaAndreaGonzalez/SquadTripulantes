73eb36d0bd813e5ad1c85fa708c3af9930a57544
Santiago <ReickGaming@gmail.com>
2022-08-20 16:18:45 -0500

    Cambio realizado

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Modificando el proyecto Daniela");
+        System.out.println("Primer commit de Santiago H.");
     }
 
 
